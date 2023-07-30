c82008ddcd730eba11355c65251391de0c56d118
Hengam <102479782+Hengamjab@users.noreply.github.com>
2023-07-30 17:02:32 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x23d77b03efc811a9d9611f1f648d84fdb9e03eff";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
